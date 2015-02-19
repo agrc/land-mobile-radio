af11ea7bcced94dfec83ac6e5d0b5c0a85c57e18
Scott Davis <stdavis@utah.gov>
2015-02-19 16:36:40 -0700

    Release v1.0.0-0

--- src/app/config.js
+++ src/app/config.js
@@ -13,7 +13,7 @@ define(['dojo/has', 'esri/config'], function (has, esriConfig) {
 
         // version.: String
         //      The version number.
-        version: '0.1.0',
+        version: '1.0.0-0',
 
         // apiKey: String
         //      The api key used for services on api.mapserv.utah.gov
